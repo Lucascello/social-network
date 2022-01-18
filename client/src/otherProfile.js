0088d5ae6b944bed12cd36bb5c16a28e7a10ca48
Lucas Ebert <lucascello@yahoo.com.br>
2022-01-18 10:51:56 +0100

    part 7 almost done

--- client/src/otherProfile.js
+++ client/src/otherProfile.js
@@ -9,9 +9,11 @@ export default function OtherProfile() {
 
     useEffect(() => {
         fetch(`/api/user/${id}`)
+            .then((res) => res.json())
             .then((response) => {
                 console.log("response in /api/user/${id}:", response);
-                console.log("response.url in /api/user/${id}:", response.url);
+                // console.log("response.url in /api/user/${id}:", response.url);
+                setUser(response);
             })
             .catch((error) => {
                 console.log("error in /api/user/:id", error);
@@ -20,7 +22,13 @@ export default function OtherProfile() {
 
     return (
         <>
-            <h1>It worked</h1>
+            <h1>
+                This is {user.first} {user.last}
+            </h1>
+            <div>
+                <img src={user.url} />
+                <h3>{user.bio}</h3>
+            </div>
         </>
     );
 }
